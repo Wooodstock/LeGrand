17dbf23ab406d4be94b49fae437ae78ceb5b28dc
Wooodstock <bertrand.heuillard@viacesi.fr>
2014-01-22 18:12:42 +0100

    Conflicts

--- ServiceLeGrand/OpenWebNetWinForm/Form1.Designer.cs
+++ ServiceLeGrand/OpenWebNetWinForm/Form1.Designer.cs
@@ -29,27 +29,42 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.buttonTest = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(81, 117);
+            this.button1.Location = new System.Drawing.Point(61, 95);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(122, 23);
+            this.button1.Size = new System.Drawing.Size(92, 19);
             this.button1.TabIndex = 0;
             this.button1.Text = "Start Services";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // buttonTest
+            // 
+            this.buttonTest.Location = new System.Drawing.Point(109, 176);
+            this.buttonTest.Margin = new System.Windows.Forms.Padding(2);
+            this.buttonTest.Name = "buttonTest";
+            this.buttonTest.Size = new System.Drawing.Size(92, 19);
+            this.buttonTest.TabIndex = 1;
+            this.buttonTest.Text = "Test";
+            this.buttonTest.UseVisualStyleBackColor = true;
+            this.buttonTest.Click += new System.EventHandler(this.buttonTest_Click);
+            // 
             // Form1
             // 
-            this.Load += new System.EventHandler(this.Form1_Load);
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(282, 253);
+            this.ClientSize = new System.Drawing.Size(212, 206);
+            this.Controls.Add(this.buttonTest);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
         }
@@ -57,6 +72,7 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button buttonTest;
     }
 }
 
--- ServiceLeGrand/ServiceLeGrand/OpenWebNet.cs
+++ ServiceLeGrand/ServiceLeGrand/OpenWebNet.cs
@@ -5,7 +5,11 @@ using System.Text;
 using System.Threading.Tasks;
 using OpenWebNetDataContract.Model;
 using InterfaceServiceLegrand;
+<<<<<<< HEAD
 using ServiceLeGrand.WallAccess;
+=======
+using System.Data;
+>>>>>>> f9fdd06dacaba4bc5c33c75268bbf471fa9d1f24
 
 namespace ServiceLeGrand
 {
@@ -163,7 +167,56 @@ namespace ServiceLeGrand
 
         public User addUser(string name, string surname, string mail, string password)
         {
-            throw new NotImplementedException();
+            CAD.SQLite db;
+
+            //TODO: Check if user deja en base
+
+            try
+            {
+                //Ajout de l'user en base
+                db = new CAD.SQLite();
+                String InsertQuery = "INSERT INTO User (Name, Surname, Mail, Password) VALUES ('" + name + "', '" + surname + "', '" + mail + "', '" + password + "');";
+                int rowsUpdated = db.ExecuteNonQuery(InsertQuery);
+
+                //Recuperation de l'id de l'user (mail doit etre unique)
+                if (rowsUpdated > 0)
+                {
+                    DataTable result;
+                    String selectQuery = "SELECT Id FROM User WHERE Mail = '" + mail + "'";
+                    result = db.GetDataTable(selectQuery);
+                    int id = 0;
+
+                    foreach (DataRow r in result.Rows)
+                    {
+                        id = int.Parse(r["Id"].ToString());
+                    }
+
+                    //Creation de lobjet User
+                    if (id != 0)
+                    {
+                        User user = new User(id, name, surname, mail, password);
+                        Console.WriteLine("New User Created");
+                        return user;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Erreur creation User");
+                        return null;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Erreur creation User");
+                    return null;
+                }
+            }
+            catch (Exception fail)
+            {
+                String error = "Erreur creation User - The following error has occurred:\n\n";
+                error += fail.Message.ToString() + "\n";
+                Console.WriteLine(error);
+                return null;
+            }
         }
 
         public bool removeUser(User user)
@@ -176,9 +229,40 @@ namespace ServiceLeGrand
             throw new NotImplementedException();
         }
 
-        public bool connectUser(string login, string password)
+        public bool connectUser(string mail, string password)
         {
-            throw new NotImplementedException();
+            CAD.SQLite db;
+            String dbPassword;
+
+            try
+            {
+                db = new CAD.SQLite();
+                DataTable result;
+                String query = "SELECT Password FROM User WHERE Mail = '" + mail + "'";
+                result = db.GetDataTable(query);
+                dbPassword = "password";
+
+                // boucle resultat reauete
+                foreach (DataRow r in result.Rows)
+                {
+                    dbPassword = r["Password"].ToString();
+                }
+
+                if (password.Equals(dbPassword))
+                {
+                    Console.WriteLine("User " + mail + " connected");
+                    //Connexionn SUCCES
+                    return true;
+                }
+            }
+            catch (Exception fail)
+            {
+                String error = "The following error has occurred:\n\n";
+                error += fail.Message.ToString() + "\n";
+                Console.WriteLine(error);
+                return false;
+            }
+            return false;
         }
 
         public bool logout()
